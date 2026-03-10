64a9c850a215264d9189fe7d8962dad20a4134ab
aariyan1586 <aariyan1586@gmail.com>
2026-03-10 13:57:26 +0530

    UC8

---
+++ src/UC8.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+import java.util.Map;
+public class UC8 {
+    public static void main(String[] args) {
+        Map<Character, String[]> patternMap = initializePatternMap();
+        renderBanner("OOPS", patternMap);
+    }
+    public static Map<Character, String[]> initializePatternMap() {
+        Map<Character, String[]> map = new HashMap<>();
+        map.put('O', new String[]{
+                "  ***  ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                "  ***  "
+        });
+        map.put('P', new String[]{
+                " *** ",
+                " *     * ",
+                " *     * ",
+                " *** ",
+                " *       ",
+                " *       ",
+                " *       "
+        });
+        map.put('S', new String[]{
+                " *** ",
+                " *       ",
+                " *       ",
+                " *** ",
+                "       * ",
+                "       * ",
+                " *** "
+        });
+        return map;
+    }
+    public static void renderBanner(String text, Map<Character, String[]> map) {
+        int height = 7;
+        for (int row = 0; row < height; row++) {
+            StringBuilder lineBuilder = new StringBuilder();
+            for (char ch : text.toCharArray()) {
+                String[] pattern = map.get(ch);
+                if (pattern != null) {
+                    lineBuilder.append(pattern[row]).append("   ");
+                }
+            }
+            System.out.println(lineBuilder.toString());
+        }
+    }
+}
